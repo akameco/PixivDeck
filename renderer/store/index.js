b014241ee10755b8ec69ef6fbb8ad1994edba2d0
akameco <akameco.t@gmail.com>
2016-07-02 10:13:42 +0900

    Support redux-devtool

--- renderer/store/index.js
+++ renderer/store/index.js
@@ -1,26 +1,5 @@
-import {createStore, applyMiddleware, compose} from 'redux';
-import createLogger from 'redux-logger';
-import createSagaMiddleware from 'redux-saga';
-import reducer from '../reducers';
-import rootSaga from '../sagas';
-
-export default function configureStore() {
-	const logger = createLogger();
-	const sagaMiddleware = createSagaMiddleware();
-
-	const store = createStore(reducer,
-		compose(applyMiddleware(sagaMiddleware, logger))
-	);
-
-	sagaMiddleware.run(rootSaga);
-
-	if (module.hot) {
-		// Enable Webpack hot module replacement for reducers
-		module.hot.accept('../reducers', () => {
-			const nextRootReducer = require('../reducers').default;
-			store.replaceReducer(nextRootReducer);
-		});
-	}
-
-	return store;
+if (process.env.NODE_ENV === 'production') {
+	module.exports = require('./configure-store.prod').default;
+} else {
+	module.exports = require('./configure-store.dev').default;
 }
