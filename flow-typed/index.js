7f44dd58aafc34a1ca617cf954a703bf02d77d96
akameco <akameco.t@gmail.com>
2016-07-05 09:44:19 +0900

    Update types

--- flow-typed/index.js
+++ flow-typed/index.js
@@ -1,7 +1,3 @@
-declare module 'react-redux' {
-	declare var exports: any;
-}
-
 declare module 'react-css-modules' {
 	declare var exports: any;
 }
@@ -10,10 +6,10 @@ declare module 'electron' {
 	declare var exports: any;
 }
 
-declare module 'react-router-redux' {
+declare module 'humps' {
 	declare var exports: any;
 }
 
-declare module 'humps' {
-	declare var exports: any;
+declare class IntersectionObserver {
+	observe(): () => void;
 }
