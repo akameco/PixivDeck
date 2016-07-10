5ca4a8a9d1f5244fe4db23425ec6a01dad911f78
akameco <akameco.t@gmail.com>
2016-07-10 20:07:35 +0900

    More ranking mode

---
+++ renderer/contains.js
@@ -0,0 +1,5 @@
+import type {RankingModeType} from './actions/type';
+
+export const rankingModes: Array<RankingModeType> = [
+	'daily', 'weekly', 'monthly', 'rookie', 'original', 'male', 'female', 'daily_r18', 'weekly_r18', 'male_r18', 'female_r18', 'r18g'
+];
