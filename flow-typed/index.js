047a8dc3dfb53dbf57536d95fcd6df1235ca108f
akameco <akameco.t@gmail.com>
2016-07-13 03:08:44 +0900

    Update type

--- flow-typed/index.js
+++ flow-typed/index.js
@@ -2,8 +2,97 @@ declare module 'react-css-modules' {
 	declare var exports: any;
 }
 
-declare module 'electron' {
+declare module 'babel-polyfill' {
 	declare var exports: any;
+}
+
+declare module 'pixiv.js' {
+	declare var exports: any;
+}
+
+declare class EventEmitter {
+	static listenerCount(emitter: EventEmitter, event: string): number;
+
+	addListener(event: string, listener: Function): EventEmitter;
+	on(event: string, listener: Function): EventEmitter;
+	once(event: string, listener: Function): EventEmitter;
+	removeListener(event: string, listener: Function): EventEmitter;
+	removeAllListeners(event?: string): EventEmitter;
+	setMaxListeners(n: number): void;
+	listeners(event: string): Function[];
+	emit(event: string, ...args: any[]): boolean;
+}
+
+declare module 'electron' {
+	declare type IpcEvent = {
+		returnValue: string,
+		sender: webContents
+	}
+
+	declare type detail = {
+		id: number,
+		url: string,
+		method: string,
+		resourceType: string,
+		timestamp: number,
+		requestHeaders: Object
+	};
+
+	declare type response = {
+		cancel?: bool,
+		requestHeaders?: Object
+	};
+
+	declare function listener(detail: detail, callback: () => void): response;
+
+	declare class WebRequest {
+		onBeforeSendHeaders(filter: Object, listener: listener): void;
+	}
+
+	declare class Session {
+		webRequest: WebRequest;
+	}
+
+	declare class webContents {
+		on(event: 'did-finish-load', listener: () => void): void;
+
+		static loadURL(url: string, options?: any): void;
+
+		getTitle(): string;
+		isLoading(): any;
+		reload(): void;
+		toggleDevTools(): void;
+		send(channel: string, arg?: any): void;
+		session: Session;
+	}
+
+	declare class BrowserWindow extends EventEmitter {
+		openDevTools(): void;
+		loadURL(url: string, options: any): void;
+		reload(): void;
+		getTitle(): string;
+		getBounds(): {x: number, y: number, width: number, height: number};
+		static on(event: 'page-title-updated' | 'close'): EventEmitter;
+		static addDevToolsExtension(path: string): void;
+		static removeDevToolsExtension(name: string): void;
+		webContents: webContents;
+	}
+
+	declare class ipcRenderer {
+		static on(channel: string, callback: (event: Event, data: Object) => void): EventEmitter;
+		static send(channel: string, arg?: any): EventEmitter;
+	}
+
+	declare function _callback(event: IpcEvent, data: Object): void;
+	declare class ipcMain {
+		static on(channel: string,
+			callback: (_callback | Promise<_callback>)): EventEmitter;
+		static send(channel: string, arg?: any): EventEmitter;
+	}
+	declare class app {
+		static on(event: 'activate' | 'ready' | 'window-all-closed', listener: () => void): void;
+		static quit(): void;
+	}
 }
 
 declare module 'humps' {
@@ -12,4 +101,12 @@ declare module 'humps' {
 
 declare class IntersectionObserver {
 	observe(): () => void;
+}
+
+declare module 'redux-logger' {
+	declare var exports: any;
+}
+
+declare module 'redux-thunk' {
+	declare var exports: any;
 }
