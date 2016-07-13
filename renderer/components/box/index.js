a69cfc6bbefbc20cb3f785b22ab259b1da19658d
akameco <akameco.t@gmail.com>
2016-07-13 15:40:09 +0900

    Separate component

--- renderer/components/box/index.js
+++ renderer/components/box/index.js
@@ -2,9 +2,9 @@
 import React, {Component} from 'react';
 import cssModules from 'react-css-modules';
 import type {WorkType, UserType} from '../../actions/type';
-import {MangaIcon} from '../icon/';
 import BoxHeader from './box-header';
 import BoxFooter from './box-footer';
+import BoxImage from './box-image';
 import styles from './box.css';
 
 type Props = {
@@ -27,26 +27,13 @@ class ImageBox extends Component {
 
 	render() {
 		const {work, user} = this.props;
-		const {title, imageUrls, caption, tags, pageCount} = work;
+		const {title, caption, tags} = work;
 		return (
 			<div styleName="base">
-				<BoxHeader
-					name={user.name}
-					account={user.account}
-					img={user.profileImageUrls.px50x50}
-					/>
+				<BoxHeader name={user.name} account={user.account} img={user.profileImageUrls.px50x50}/>
 				{title}
 				{caption}
-				<div styleName="image-wrap">
-					{pageCount > 1 &&
-						<MangaIcon/>
-					}
-					<img
-						src={imageUrls.medium}
-						onClick={this.handleClick}
-						/>
-				</div>
-				<br/>
+				<BoxImage work={work} onClick={this.handleClick}/>
 				<BoxFooter tags={tags} onClickTag={this.props.onClickTag}/>
 			</div>
 		);
