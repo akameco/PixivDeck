feed69a92a2dcc7c5ad6cd003af6c4039ebe24b5
akameco <akameco.t@gmail.com>
2016-07-16 00:08:45 +0900

    Add close reducer

--- renderer/reducers/manage.js
+++ renderer/reducers/manage.js
@@ -22,15 +22,15 @@ export default function (state: Manage = initManageState, action: Action): Manag
 		case 'LOGOUT':
 			return {...state, isLogin: false};
 		case 'OPEN_IMAGE_VIEW':
-			return {...state, isImageView: Boolean(state.currentWorkId)};
+			return {...state, isImageView: Boolean(state.currentWorkId), isMangaView: false, isDropdown: false};
 		case 'CLOSE_IMAGE_VIEW':
 			return {...state, isImageView: false};
 		case 'OPEN_MANGA_PREVIEW':
-			return {...state, isMangaView: Boolean(state.currentWorkId)};
+			return {...state, isMangaView: Boolean(state.currentWorkId), isImageView: false, isDropdown: false};
 		case 'CLOSE_MANGA_PREVIEW':
 			return {...state, isMangaView: false};
 		case 'OPEN_MODAL':
-			return {...state, isModal: true};
+			return {...state, isModal: true, modalType: action.modal, isMangaView: false, isImageView: false, isDropdown: false};
 		case 'CLOSE_MODAL':
 			return {...state, isModal: false};
 		case 'OPEN_DROPDOWN':
