371f5af52155362c950d9ecf74dede7df699d0ce
akameco <akameco.t@gmail.com>
2016-07-17 06:00:28 +0900

    Add Full Size Preview

--- renderer/components/illust-preview/lazy-img.js
+++ renderer/components/illust-preview/lazy-img.js
@@ -1,17 +1,23 @@
 // @flow
 import React, {Component} from 'react';
 import CSSModules from 'react-css-modules';
+import {findDOMNode} from 'react-dom';
 import styles from './lazy-img.css';
 
-function createStyle(width: number, height: number): {
+type Size = {
 	width: number | 'auto',
 	height: number | 'auto'
-} {
+};
+
+function calcSize(width: number, height: number): Size {
 	const {innerWidth, innerHeight} = window;
 	if (height > innerHeight && width > innerWidth) {
 		return (width * innerHeight < height * innerWidth) ?
 			{width: 'auto', height: innerHeight} :
-			{width: innerWidth, height: 'auto'};
+			{
+				width: innerWidth,
+				height: 'auto'
+			};
 	}
 	if (height > innerHeight) {
 		return {width: 'auto', height: innerHeight};
@@ -32,9 +38,53 @@ type Props = {
 	onLoad: () => void
 };
 
+type State = {
+	isClicked: bool,
+	fromMarginTop: number,
+	toMarginTop: number
+};
+
 @CSSModules(styles)
 export default class LazyImg extends Component {
 	props: Props;
+	state: State;
+	to: Component<*, *, *>
+	from: Component<*, *, *>
+
+	constructor(props: Props) {
+		super(props);
+		this.state = {
+			isClicked: false,
+			fromMarginTop: 0,
+			toMarginTop: 0
+		};
+	}
+
+	componentDidMount() {
+		const to = this.calcMarginTop(findDOMNode(this.to));
+		const from = this.calcMarginTop(findDOMNode(this.from));
+		this.setState({
+			toMarginTop: to,
+			fromMarginTop: from
+		});
+	}
+
+	componentWillUpdate(nextProps: Props, nextState: State) {
+		const to = this.calcMarginTop(findDOMNode(this.to));
+		const from = this.calcMarginTop(findDOMNode(this.from));
+		if (
+			this.props.isLoaded !== nextProps.isLoaded ||
+			nextState.isClicked !== this.state.isClicked ||
+			nextState.fromMarginTop !== from ||
+			nextState.toMarginTop !== to
+		) {
+			console.log(from, to);
+			this.setState({
+				toMarginTop: to,
+				fromMarginTop: from
+			});
+		}
+	}
 
 	handleLoad = () => {
 		const img = new Image();
@@ -44,22 +94,72 @@ export default class LazyImg extends Component {
 		img.src = this.props.to;
 	}
 
+	calcMarginTop(node: HTMLElement): number {
+		if (node) {
+			const height = node && node.clientHeight;
+			if (window.innerHeight > height) {
+				const top = (window.innerHeight - height) / 2;
+				return top;
+			}
+		}
+		return 0;
+	}
+
+	selectStyle(isLoaded: bool, isClick: bool): string {
+		if (isLoaded && isClick) {
+			return 'isClick';
+		}
+		if (isLoaded) {
+			return 'loaded';
+		}
+		return 'loading';
+	}
+
+	handleClick = (event: SyntheticEvent) => { // eslint-disable-line no-undef
+		event.stopPropagation();
+		this.setState({isClicked: !this.state.isClicked});
+	}
+
 	render() {
 		const {width, height, isLoaded} = this.props;
-		const style = isLoaded ? 'loaded' : 'loading';
-		const fromStyle = createStyle(width, height);
+		const style = this.selectStyle(isLoaded, this.state.isClicked);
+		const toStyle = {marginTop: `${this.state.toMarginTop}px`};
+		const fromStyle = Object.assign(
+			{},
+			calcSize(width, height),
+			{marginTop: `${this.state.fromMarginTop}px`}
+		);
 
 		if (this.props.isLoaded) {
-			return <img src={this.props.to} styleName={style}/>;
+			return (
+				<div styleName="wrap">
+					<img
+						src={this.props.to}
+						width={width}
+						height={height}
+						style={toStyle}
+						styleName={style}
+						onClick={this.handleClick}
+						ref={c => {
+							this.to = c;
+						}}
+						/>
+				</div>
+			);
 		}
 
 		return (
-			<img
-				src={this.props.from}
-				styleName="from"
-				style={fromStyle}
-				onLoad={this.handleLoad}
-				/>
+			<div styleName="wrap">
+				<img
+					src={this.props.from}
+					styleName="from"
+					style={fromStyle}
+					onLoad={this.handleLoad}
+					ref={c => {
+						this.from = c;
+					}}
+					/>
+			</div>
 		);
 	}
 }
