b583a4b3df0a406885a34f75f9a07c8d1b7fb96c
akameco <akameco.t@gmail.com>
2016-07-16 23:02:10 +0900

    Add lazy load image

--- renderer/components/box/box-image.js
+++ renderer/components/box/box-image.js
@@ -1,5 +1,6 @@
 // @flow
 import React, {Component} from 'react';
+import {findDOMNode} from 'react-dom';
 import CSSModules from 'react-css-modules';
 import type {Work} from '../../types';
 import {MangaIcon} from '../icon/';
@@ -10,21 +11,86 @@ type Props = {
 	onClick: () => void
 };
 
+type State = {
+	isVisible: bool,
+	isLoaded: bool
+};
+
 @CSSModules(styles)
 export default class BoxImage extends Component {
 	props: Props;
+	state: State;
+	target: Component<*, *, *>;
+	io: Object;
+
+	constructor(props: Props) {
+		super(props);
+		this.state = {
+			isVisible: false,
+			isLoaded: false
+		};
+	}
+
+	shouldComponentUpdate(nextProps: Props, nextState: State) {
+		if (this.state.isLoaded !== nextState.isLoaded) {
+			return true;
+		}
+		if (this.state.isVisible !== nextState.isVisible) {
+			return true;
+		}
+		return false;
+	}
+
+	componentDidMount() {
+		const target = this.target;
+		this.io = new IntersectionObserver(entries => { // eslint-disable-line no-undef
+			const intersectionRatio = entries[0].intersectionRatio;
+			if (intersectionRatio <= 0) {
+				this.setState({isVisible: false});
+			}
+			this.update();
+		}, {
+			rootMargin: '500px'
+		});
+		this.io.observe(findDOMNode(target));
+	}
+
+	componentWillUnmount() {
+		this.io.unobserve(findDOMNode(this.target));
+	}
+
+	update() {
+		this.setState({isVisible: true});
+
+		const img = new Image();
+		img.onload = () => {
+			this.setState({isLoaded: true});
+		};
+		img.src = this.props.work.imageUrls.medium;
+	}
+
+	handleRefs = (c: Component<*, *, *>) => {
+		this.target = c;
+	}
 
 	render() {
 		const {imageUrls, pageCount} = this.props.work;
 		return (
-			<div styleName="base">
+			<div
+				ref={this.handleRefs}
+				styleName="base"
+				>
 				{pageCount > 1 &&
 					<MangaIcon/>
 				}
-				<img
-					src={imageUrls.medium}
-					onClick={this.props.onClick}
-					/>
+				{this.state.isVisible && this.state.isLoaded ?
+					<img
+						styleName="loaded"
+						src={imageUrls.medium}
+						onClick={this.props.onClick}
+						/> :
+					<img height={200}/>
+				}
 			</div>
 		);
 	}
