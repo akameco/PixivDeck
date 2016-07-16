d8a6a481322ee2e48e031a370d2feef2d7120852
akameco <akameco.t@gmail.com>
2016-07-17 07:45:44 +0900

    Fix image size

--- renderer/components/manga-preview/multi-preview.js
+++ renderer/components/manga-preview/multi-preview.js
@@ -2,6 +2,7 @@
 import React, {Component} from 'react';
 import CSSModules from 'react-css-modules';
 import type {Page} from '../../types';
+import {CloseButton} from '../button';
 import styles from './multi-preview.css';
 
 type Props = {
@@ -23,6 +24,10 @@ export default class MultiPreview extends Component {
 		});
 		return (
 			<div styleName="base" onClick={this.props.onClose}>
+				<CloseButton
+					onClick={this.props.onClose}
+					style={{position: 'fixed'}}
+					/>
 				{imgs}
 			</div>
 		);
