cb2e7125bccdf5a12fbc8f3a97d8be716e33524a
akameco <akameco.t@gmail.com>
2016-07-18 20:09:54 +0900

    :beer: Update Smart Header

---
+++ renderer/components/header/header.js
@@ -0,0 +1,46 @@
+// @flow
+import React, {Component} from 'react';
+import CSSModules from 'react-css-modules';
+import type {Manage} from '../../types';
+import {AddIcon, SearchIcon, SettingsIcon} from '../icon';
+import Dropdwon from '../../container/smart-dropdown';
+import styles from './header.css';
+
+type Props = {
+	manage: Manage,
+	onClickAdd: () => void,
+	onToggleDropdown: () => void,
+	onOpenSearchModal: () => void
+};
+
+@CSSModules(styles)
+export default class Header extends Component {
+	props: Props;
+
+	shouldComponentUpdate(nextProps: Props) {
+		return this.props.manage.isDropdown !== nextProps.manage.isDropdown;
+	}
+
+	render() {
+		return (
+			<header styleName="base">
+				<div styleName="top">
+					<a styleName="add" onClick={this.props.onClickAdd}>
+						<AddIcon/>
+					</a>
+					<a styleName="search" onClick={this.props.onOpenSearchModal}>
+						<SearchIcon/>
+					</a>
+				</div>
+				<div styleName="bottom">
+					<a onClick={this.props.onToggleDropdown}>
+						<SettingsIcon/>
+					</a>
+				</div>
+				{this.props.manage.isDropdown &&
+					<Dropdwon/>
+				}
+			</header>
+		);
+	}
+}
