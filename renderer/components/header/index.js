d16aa0766b390cde9de2a039a565820bbf37fc06
akameco <akameco.t@gmail.com>
2016-07-18 20:09:54 +0900

    :beer: Update Smart Header

--- renderer/components/header/index.js
+++ renderer/components/header/index.js
@@ -1,72 +1,19 @@
 // @flow
-import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import CSSModules from 'react-css-modules';
-import type {Dispatch, State, Manage} from '../../types';
-import {
-	closeDropdown,
-	toggleDropdown,
-	openModal
-} from '../../actions/manage';
-import {AddIcon, SearchIcon, SettingsIcon} from '../icon';
-import Dropdwon from '../dorpdown';
-import styles from './header.css';
-
-type Props = {
-	manage: Manage,
-	dispatch: Dispatch
-};
-
-@CSSModules(styles)
-class Header extends Component {
-	props: Props;
-
-	shouldComponentUpdate(nextProps) {
-		return this.props.manage.isDropdown !== nextProps.manage.isDropdown;
-	}
-
-	handleAddClick = () => {
-		this.props.dispatch(openModal());
-	}
-
-	handleOpenSearchModal = () => {
-		this.props.dispatch(openModal('SEARCH'));
-	}
-
-	handleDropdown = () => {
-		this.props.dispatch(toggleDropdown());
-	}
-
-	handleClose = () => {
-		this.props.dispatch(closeDropdown());
-	}
-
-	render() {
-		return (
-			<header styleName="base">
-				<div styleName="top">
-					<a styleName="add" onClick={this.handleAddClick}>
-						<AddIcon/>
-					</a>
-					<a styleName="search" onClick={this.handleOpenSearchModal}>
-						<SearchIcon/>
-					</a>
-				</div>
-				<div styleName="bottom">
-					<a onClick={this.handleDropdown}>
-						<SettingsIcon/>
-					</a>
-				</div>
-				{this.props.manage.isDropdown &&
-					<Dropdwon/>
-				}
-			</header>
-		);
-	}
-}
+import type {Dispatch, State} from '../../types';
+import {toggleDropdown, openModal} from '../../actions/manage';
+import Header from './header';
 
 function mapStateToProps(state: State) {
 	return {manage: state.manage};
 }
 
-export default connect(mapStateToProps)(Header);
+function mapDispatchToProps(dispatch: Dispatch) {
+	return {
+		onClickAdd: () => dispatch(openModal()),
+		onToggleDropdown: () => dispatch(toggleDropdown()),
+		onOpenSearchModal: () => dispatch(openModal('SEARCH'))
+	};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
