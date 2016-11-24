d69c3408c498d9005b170b0e7cb96d8dc69acdef
akameco <akameco.t@gmail.com>
2016-11-24 17:46:29 +0900

    Add illustFollow Column

--- app/components/modal/select/select-modal.jsx
+++ app/components/modal/select/select-modal.jsx
@@ -46,6 +46,11 @@ export default class SelectColumnModal extends Component {
 		this.props.onSelect({type: 'favoriteIllusts', opts: {restrict: publicity}}, title);
 	}
 
+	handleAddIllustFollow = (publicity: 'private' | 'public') => {
+		const title = {private: '新着 非公開', public: '新着 公開'}[publicity];
+		this.props.onSelect({type: 'illustFollow', opts: {restrict: publicity}}, title);
+	}
+
 	handleAddHistory = () => {
 		this.props.onSelect({type: 'history'}, 'history');
 	}
@@ -61,6 +66,8 @@ export default class SelectColumnModal extends Component {
 
 		const onClickPublic = () => this.handleAddFavorite('public');
 		const onClickPrivate = () => this.handleAddFavorite('private');
+		const onClickillustFollowPublic = () => this.handleAddIllustFollow('public');
+		const onClickillustFollowPrivate = () => this.handleAddIllustFollow('private');
 
 		return (
 			<div styleName="wrap">
@@ -82,6 +89,19 @@ export default class SelectColumnModal extends Component {
 						<li>
 							<a onClick={onClickPrivate}>
 								非公開ブックマーク
+							</a>
+						</li>
+					</ul>
+					<div styleName="kind">新着</div>
+					<ul styleName="list">
+						<li>
+							<a onClick={onClickillustFollowPublic()}>
+								公開
+							</a>
+						</li>
+						<li>
+							<a onClick={onClickillustFollowPrivate()}>
+								非公開
 							</a>
 						</li>
 					</ul>
