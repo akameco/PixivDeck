d930c41c17540e917f91687f47bd0d46851d0bdc
akameco <akameco.t@gmail.com>
2016-11-28 16:49:36 +0900

    Fix Profile type definition & show twitter link

--- app/types/user.js
+++ app/types/user.js
@@ -11,20 +11,20 @@ export type User = {|
 |};
 
 export type Profile = {
-	background_image_url: string,
+	backgroundImageUrl: string,
 	birth: string,
 	gender: string,
-	is_premium: bool,
+	isPremium: bool,
 	region: string,
-	total_follow_users: number,
-	total_follower: number,
-	total_illust_bookmarks_public: number,
-	total_illusts: number,
-	total_manga: number,
-	total_mypixiv_users: number,
-	total_novels: number,
-	twitter_accounts: string,
-	twitter_url: ?string,
+	totalFollowUsers: number,
+	totalFollower: number,
+	totalIllustBookmarksPublic: number,
+	totalIllusts: number,
+	totalManga: number,
+	totalMypixivUsers: number,
+	totalNovels: number,
+	twitterAccounts: string,
+	twitterUrl: ?string,
 	webpage: string
 };
 
