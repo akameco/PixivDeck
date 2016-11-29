7a44a1e75ee6ec06d781e7d6d548d14eb80f1776
akameco <akameco.t@gmail.com>
2016-11-29 15:33:05 +0900

    Use const

--- app/actions/manage.js
+++ app/actions/manage.js
@@ -2,58 +2,23 @@
 import type {ThunkAction} from 'redux'; // eslint-disable-line
 import type {Action, ModalType} from '../types';
 
-export function openMangaPreview(): Action {
-	return {type: 'OPEN_MANGA_PREVIEW'};
-}
+export const openModal = (modal?: ModalType = 'DEFAULT'): Action => (
+	{type: 'OPEN_MODAL', modal}
+);
+export const closeModal = (): Action => ({type: 'CLOSE_MODAL'});
 
-export function close(): Action {
-	return {type: 'CLOSE_ALL'};
-}
+export const openDrawer = (id: number): Action => ({type: 'OPEN_DRAWER', id});
+export const closeDrawer = (): Action => ({type: 'CLOSE_DRAWER'});
 
-export function openModal(modal: ModalType = 'DEFAULT'): Action {
-	return {type: 'OPEN_MODAL', modal};
-}
+export const openDropdown = (): Action => ({type: 'OPEN_DROPDOWN'});
+export const toggleDropdown = (): Action => ({type: 'TOGGLE_DROPDOWN'});
+export const closeDropdown = (): Action => ({type: 'CLOSE_DROPDOWN'});
 
-export function closeModal(): Action {
-	return {type: 'CLOSE_MODAL'};
-}
+export const openImageView = (): Action => ({type: 'OPEN_IMAGE_VIEW'});
+export const closeImageView = (): Action => ({type: 'CLOSE_IMAGE_VIEW'});
 
-export function openDrawer(id: number): Action {
-	return {type: 'OPEN_DRAWER', id};
-}
+export const openMangaPreview = (): Action => ({type: 'OPEN_MANGA_PREVIEW'});
+export const closeMnagaPreview = (): Action => ({type: 'CLOSE_MANGA_PREVIEW'});
 
-export function closeDrawer(): Action {
-	return {type: 'CLOSE_DRAWER'};
-}
-
-export function openDropdown(): Action {
-	return {type: 'OPEN_DROPDOWN'};
-}
-
-export function toggleDropdown(): Action {
-	return {type: 'TOGGLE_DROPDOWN'};
-}
-
-export function closeDropdown(): Action {
-	return {type: 'CLOSE_DROPDOWN'};
-}
-
-export function openImageView(): Action {
-	return {type: 'OPEN_IMAGE_VIEW'};
-}
-
-export function closeImageView(): Action {
-	return {type: 'CLOSE_IMAGE_VIEW'};
-}
-
-export function closeMnagaPreview(): Action {
-	return {type: 'CLOSE_MANGA_PREVIEW'};
-}
-
-export function startImgLoading(): Action {
-	return {type: 'START_IMG_LOADING'};
-}
-
-export function finishImgLoaded(): Action {
-	return {type: 'SET_IMG_LOADED'};
-}
+export const startImgLoading = (): Action => ({type: 'START_IMG_LOADING'});
+export const finishImgLoaded = (): Action => ({type: 'SET_IMG_LOADED'});
