ff5b66b90bde9d440d42d0051d2b2a4a3d0de2f9
akameco <akameco.t@gmail.com>
2016-11-29 10:12:42 +0900

    Remove Loading Icon

--- app/components/icon/index.jsx
+++ app/components/icon/index.jsx
@@ -39,17 +39,3 @@ export default class Icon extends Component {
 		);
 	}
 }
-
-export const LoadingIcon = () => (
-	<svg viewBox="0 0 24 30">
-		<rect x="0" y="0" width="4" height="20" fill="#333">
-			<animate attributeName="opacity" attributeType="XML" values="1; .2; 1" begin="0s" dur="0.6s" repeatCount="indefinite"/>
-		</rect>
-		<rect x="7" y="0" width="4" height="20" fill="#333">
-			<animate attributeName="opacity" attributeType="XML" values="1; .2; 1" begin="0.2s" dur="0.6s" repeatCount="indefinite"/>
-		</rect>
-		<rect x="14" y="0" width="4" height="20" fill="#333">
-			<animate attributeName="opacity" attributeType="XML" values="1; .2; 1" begin="0.4s" dur="0.6s" repeatCount="indefinite"/>
-		</rect>
-	</svg>
-);
