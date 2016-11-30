4920320c8bcea6e6d1d028d85641e136e4f9205d
akameco <akameco.t@gmail.com>
2016-11-30 15:16:23 +0900

    Use initState

---
+++ app/default-state.js
@@ -0,0 +1,45 @@
+// @flow
+import type {ColumnType as Column} from './types'
+
+const hour = 1000 * 60 * 60
+
+const initState: Array<Column> = [
+	{
+		id: 1,
+		endpoint: 'illustRanking',
+		timer: hour,
+		title: 'デイリーランキング',
+		query: {
+			opts: {
+				mode: 'day',
+				offset: 0,
+			},
+		},
+	},
+	{
+		id: 2,
+		endpoint: 'illustRanking',
+		timer: hour,
+		title: 'ウィークリーランキング',
+		query: {
+			opts: {
+				mode: 'week',
+				offset: 0,
+			},
+		},
+	},
+	{
+		id: 3,
+		endpoint: 'illustRanking',
+		timer: hour,
+		title: 'マンスリーランキング',
+		query: {
+			opts: {
+				mode: 'month',
+				offset: 0,
+			},
+		},
+	},
+]
+
+export default initState
