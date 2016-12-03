09a471d47b8f711b71769193c87802725bd573c8
akameco <akameco.t@gmail.com>
2016-12-03 13:25:09 +0900

    Update

--- app/components/drawer/UserDrawerContainer.js
+++ app/components/drawer/UserDrawerContainer.js
@@ -9,7 +9,7 @@ import type {
 	Illust,
 } from '../../types'
 import {fetchUserDetail} from '../../actions'
-import {fetchDrawerIllust} from '../../actions/drawer'
+import {fetchDrawerIllust, fetchDrawerManga} from '../../actions/drawer'
 import {getCurrentUser, getDrawerIllusts, getDrawerMangas} from '../../reducers'
 import Loading from '../common/Loading'
 import UserDrawer from './UserDrawer'
@@ -34,8 +34,8 @@ class UserDrawerContainer extends Component {
 
 		Promise.all([
 			dispatch(fetchUserDetail(id)),
-			dispatch(fetchDrawerIllust(id, 'illust')),
-			dispatch(fetchDrawerIllust(id, 'manga')),
+			dispatch(fetchDrawerIllust(id)),
+			dispatch(fetchDrawerManga(id)),
 		])
 	}
 
--- app/reducers/drawer.js
+++ app/reducers/drawer.js
@@ -7,24 +7,24 @@ const initState = {
 	mangas: [],
 	user: null,
 	profile: null,
+	nextIllustUrl: null,
+	nextMangaUrl: null,
 }
 
 export default function drawer(state: Drawer = initState, action: Action): Drawer {
 	switch (action.type) {
 		case 'DRAWER_ADD_ILLUSTS':
-			return {
-				...state,
-				illusts: [...state.illusts, ...action.ids],
-			}
+			return {...state, illusts: [...state.illusts, ...action.ids]}
 		case 'DRAWER_ADD_MANGAS':
-			return {
-				...state,
-				mangas: [...state.mangas, ...action.ids],
-			}
+			return {...state, mangas: [...state.mangas, ...action.ids]}
 		case 'DRAWER_ADD_USER':
 			return {...state, user: action.user}
 		case 'DRAWER_ADD_PROFILE':
 			return {...state, profile: action.profile}
+		case 'DRAWER_SET_NEXT_ILLUST_URL':
+			return {...state, nextIllustUrl: action.url}
+		case 'DRAWER_SET_NEXT_MANGA_URL':
+			return {...state, nextMangaUrl: action.url}
 		case 'CLOSE_DRAWER':
 			return {...initState}
 		default:
