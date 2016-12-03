ce6d68545849f7a4914419966a7248cf90b86fa5
akameco <akameco.t@gmail.com>
2016-12-03 18:12:33 +0900

    Refactor login modal

--- app/components/modal/login/index.js
+++ app/components/modal/login/index.js
@@ -4,16 +4,12 @@ import type {Dispatch, State} from '../../../types'
 import {login} from '../../../actions'
 import LoginModal from './LoginModal'
 
-function mapStateToProps(state: State) {
-	return {...state.auth}
-}
+const mapStateToProps = ({auth}: State) => ({...auth})
 
-function mapDispatchToProps(dispatch: Dispatch) {
-	return {
-		onClick: (username, password) => {
-			dispatch(login(username, password))
-		},
-	}
-}
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+	onClick(username, password) {
+		dispatch(login(username, password))
+	},
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(LoginModal)
