8c64a5ee10c1f08b9aa9d28334f4e602bc22e80c
akameco <akameco.t@gmail.com>
2016-12-03 14:49:53 +0900

    Move store wrapper

--- app/store/configure-store.dev.js
+++ app/store/configure-store.dev.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk'
 import createLogger from 'redux-logger'
 import reducer from '../reducers'
 import middlewares from '../middlewares'
-import storeWrapper from '../store-wrapper'
+import storeWrapper from './wrapper'
 
 export default function configureStore(initialState: Object) {
 	const logger = createLogger({
