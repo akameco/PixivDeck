11ea1fe243f7d44211f2300c94ea1c1e57b339d1
akameco <akameco.t@gmail.com>
2016-12-03 19:17:50 +0900

    Refactor manga preview

---
+++ app/components/preview/manga/MultiPreview.js
@@ -0,0 +1,32 @@
+// @flow
+import React, {Component} from 'react'
+import type {MetaPages} from '../../../types'
+import CloseButton from '../../common/CloseButton'
+import styles from './MultiPreview.css'
+
+type Props = {
+	pages: MetaPages,
+	onClose: () => void
+};
+
+export default class MultiPreview extends Component {
+	props: Props;
+
+	render() {
+		const {pages, onClose} = this.props
+		const imgs = pages.map(page =>
+			<div className={styles.item} key={page.imageUrls.medium}>
+				<img src={page.imageUrls.large}/>
+			</div>
+		)
+		return (
+			<div className={styles.base} onClick={onClose}>
+				<CloseButton
+					onClick={onClose}
+					style={{position: 'fixed'}}
+					/>
+				{imgs}
+			</div>
+		)
+	}
+}
