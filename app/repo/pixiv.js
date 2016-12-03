f2b863024fa9bdf8ec57f01c289b7cfd26e99993
akameco <akameco.t@gmail.com>
2016-12-03 12:51:47 +0900

    Move schema file

--- app/repo/pixiv.js
+++ app/repo/pixiv.js
@@ -3,12 +3,12 @@ import url from 'url'
 import {normalize} from 'normalizr'
 import camelizeKeys from 'camelcase-keys'
 import PixivAppApi from 'pixiv-app-api'
-import schemas from '../schemas'
+import schema from './schema'
 
 const pixiv = new PixivAppApi()
 
 export const normalizeIllusts = (res: Object) =>
-	normalize(res.illusts, schemas.ILLUSTS)
+	normalize(res.illusts, schema.ILLUSTS)
 
 export const parseUrl = (nextUrl: string) =>
 	camelizeKeys(url.parse(nextUrl, true).query)
