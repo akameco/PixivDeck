d0ff941d5cd22799ec3474bb4acdde33dee47a61
akameco <akameco.t@gmail.com>
2016-12-05 19:06:30 +0900

    Refactor action

--- app/types/drawer.js
+++ app/types/drawer.js
@@ -16,6 +16,5 @@ export type DrawerAction =
 	| {|type: 'DRAWER_ADD_ILLUSTS', ids: number[], drawerType: DrawerType|}
 	| {|type: 'DRAWER_ADD_USER', user: User|}
 	| {|type: 'DRAWER_ADD_PROFILE', profile: Profile|}
-	| {|type: 'DRAWER_SET_NEXT_ILLUST_URL', url: string|}
-	| {|type: 'DRAWER_SET_NEXT_MANGA_URL', url: string|}
+	| {|type: 'DRAWER_SET_NEXT_URL', url: string, drawerType: DrawerType|}
 ;
