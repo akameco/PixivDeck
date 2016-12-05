c6aa210f2c6941bdda0ae2c2465ca5ca35a67694
akameco <akameco.t@gmail.com>
2016-12-05 17:16:54 +0900

    Update Infinite component

--- app/components/common/Infinite.js
+++ app/components/common/Infinite.js
@@ -1,58 +1,50 @@
 // @flow
 import React, {Component} from 'react'
 import {findDOMNode} from 'react-dom'
+import Loading from '../common/Loading'
 import styles from './Infinite.css'
 
 type Props = {
 	rootMargin?: string,
 	onIntersect: () => void,
 	children?: any,
+	root?: any,
 	style?: Object
 };
 
 export default class Infinite extends Component {
 	props: Props;
 	sentinel: Component<*, *, *>;
-	root: Component<*, *, *>;
 	io: Object;
 
 	componentDidMount() {
 		const sentinel = this.sentinel
+		const {root, rootMargin, onIntersect} = this.props
+		const defaultOpts = {rootMargin: rootMargin || '1000px'}
+		const opts = root ? defaultOpts : {...defaultOpts, root}
 		this.io = new IntersectionObserver(entries => { // eslint-disable-line no-undef
 			if (entries[0].intersectionRatio <= 0) {
 				return
 			}
 			requestAnimationFrame(() => {
-				this.handleOnIntersect()
+				onIntersect()
 			})
-		}, {
-			root: this.root,
-			rootMargin: this.props.rootMargin || '1000px',
-		})
+		}, opts)
 		this.io.observe(findDOMNode(sentinel))
-	}
-
-	handleOnIntersect() {
-		this.props.onIntersect()
 	}
 
 	render() {
 		const {style, children} = this.props
 		return (
-			<div
-				ref={c => { // eslint-disable-line react/jsx-no-bind
-					this.root = c
-				}}
-				style={style}
-				className={styles.base}
-				>
+			<div style={style} className={styles.wrap}>
 				{children}
 				<div
 					ref={c => { // eslint-disable-line react/jsx-no-bind
 						this.sentinel = c
 					}}
-					style={{height: 100}}
-					/>
+					>
+					<Loading wrapStyle={{background: '#222426', display: 'flex'}}/>
+				</div>
 			</div>
 		)
 	}
