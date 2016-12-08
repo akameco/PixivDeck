18543d3a3a6fa87172cb734743fef95c6a09f5ff
akameco <akameco.t@gmail.com>
2016-12-08 23:25:17 +0900

    Kiss

--- app/components/box/index.js
+++ app/components/box/index.js
@@ -1,5 +1,7 @@
 // @flow
+import {shell, remote, ipcRenderer} from 'electron'
 import React, {Component} from 'react'
+import {download} from 'electron-dl'
 import {connect} from 'react-redux'
 import type {Dispatch, State} from '../../types/'
 import type {Illust} from '../../types/illust'
@@ -15,6 +17,8 @@ import {
 } from '../../actions'
 import Box from './Box'
 
+const {Menu, MenuItem} = remote
+
 type Props = {
 	illust: Illust,
 	user: User,
@@ -23,7 +27,7 @@ type Props = {
 	addBookmark: (id: number, isPublic: bool) => void,
 };
 
-class SmartBox extends Component {
+class BoxContainer extends Component {
 	props: Props;
 
 	handleTagClick = (tag: string) => {
@@ -45,19 +49,107 @@ class SmartBox extends Component {
 		}
 	}
 
+	handleContextMenu = (e: Event) => {
+		e.preventDefault()
+
+		const {illust, user, addBookmark} = this.props
+		const {id, title, imageUrls, metaSinglePage} = illust
+		const img = imageUrls.large
+		const name = user.name
+
+		const menu = new Menu()
+
+		menu.append(new MenuItem({
+			label: 'オリジナルサイズの画像を保存',
+			click(item, win) {
+				download(win, img)
+			},
+		}))
+
+		menu.append(new MenuItem({type: 'separator'}))
+
+		menu.append(new MenuItem({
+			label: 'このユーザの情報を見る',
+			click() {
+				this.handleClickUser()
+			},
+		}))
+
+		menu.append(new MenuItem({type: 'separator'}))
+
+		menu.append(new MenuItem({
+			label: 'ブックマーク',
+			click: () => {
+				addBookmark(id, true)
+			},
+		}))
+
+		menu.append(new MenuItem({
+			label: '非公開ブックマーク',
+			click: () => {
+				addBookmark(id, false)
+			},
+		}))
+
+		menu.append(new MenuItem({type: 'separator'}))
+
+		menu.append(new MenuItem({
+			label: 'Twitterで共有',
+			click() {
+				const encodedTitle = encodeURIComponent(title)
+				const encodedName = encodeURIComponent(name)
+				const url = `https://twitter.com/intent/tweet?original_referer=http%3A%2F%2Fwww.pixiv.net%2Fmember_illust.php%3Fmode%3Dmedium%26illust_id%3D${id}&ref_src=twsrc%5Etfw&text=${encodedTitle}%20%7C%20${encodedName}%20%23pixiv%20%23PixivDeck&tw_p=tweetbutton&url=http%3A%2F%2Fwww.pixiv.net%2Fmember_illust.php%3Fillust_id%3D${id}%26mode%3Dmedium`
+				ipcRenderer.send('tweet', url)
+			},
+		}))
+
+		menu.append(new MenuItem({
+			label: 'pixivで開く',
+			click() {
+				shell.openExternal(`http://www.pixiv.net/member_illust.php?mode=medium&illust_id=${id}`)
+			},
+		}))
+
+		menu.append(new MenuItem({type: 'separator'}))
+
+		menu.append(new MenuItem({
+			label: '壁紙に設定',
+			click() {
+				const img = metaSinglePage.originalImageUrl
+				if (img) {
+					ipcRenderer.send('wallpaper', img)
+				} else {
+					ipcRenderer.send('wallpaper', imageUrls.large)
+				}
+			},
+		}))
+
+		menu.popup(remote.getCurrentWindow())
+	}
+
 	render() {
+		const {
+			user,
+			illust,
+			isIllustOnly,
+		} = this.props
+
 		return (
 			<Box
-				user={this.props.user}
-				illust={this.props.illust}
-				isIllustOnly={this.props.isIllustOnly}
+				user={user}
+				illust={illust}
+				isIllustOnly={isIllustOnly}
 				onClick={this.handleClick}
-				addBookmark={this.props.addBookmark}
 				onClickUser={this.handleClickUser}
 				onClickTag={this.handleTagClick}
+				onContextMenu={this.handleContextMenu}
 				/>
 		)
 	}
+}
+
+const setWallpaper = (id: number) => {
+	// stateを取って画像を特定
 }
 
 const mapStateToProps = (state: State, {illust}: Props) => {
@@ -76,4 +168,4 @@ const mapDispatchToProps = (dispatch: Dispatch) => {
 	}
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(SmartBox)
+export default connect(mapStateToProps, mapDispatchToProps)(BoxContainer)
