e1b8c59c36d33377e8c202db18b5e953edbf0536
akameco <akameco.t@gmail.com>
2016-12-09 01:16:44 +0900

    Remove history

--- app/types/index.js
+++ app/types/index.js
@@ -2,7 +2,6 @@
 import type {Store as ReduxStore, Dispatch as ReduxDispatch} from 'redux'; // eslint-disable-line
 import type {ColumnType, ColumnAction} from './column'
 import type {Manage, ManageAction} from './manage'
-import type {History, HistoryAction} from './history'
 import type {Filter, FilterAction} from './filter'
 import type {ApiAction} from './api'
 import type {Illusts} from './illust'
@@ -28,7 +27,6 @@ export type Action =
 	| ManageAction
 	| ApiAction
 	| FilterAction
-	| HistoryAction
 	| AuthAction
 	| ConfigAction
 	| DrawerAction
@@ -40,7 +38,6 @@ export type State = {
 	entities: Entities,
 	manage: Manage,
 	filter: Filter,
-	history: History,
 	config: Config,
 	illustById: Illusts,
 	userById: Users,
