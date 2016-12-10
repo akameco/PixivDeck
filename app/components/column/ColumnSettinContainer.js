ef015a05a45489bbc715fcd66aec643e65f7bcd8
akameco <akameco.t@gmail.com>
2016-12-11 04:48:54 +0900

    Tweak

--- app/components/column/ColumnSettinContainer.js
+++ app/components/column/ColumnSettinContainer.js
@@ -4,10 +4,8 @@ import type {Dispatch} from '../../types'
 import {setColumnMinBookmarks} from '../../actions'
 import ColumnSetting from './ColumnSetting'
 
-export default connect(undefined, (dispatch: Dispatch, {id}: {id: number}) => {
-	return {
-		setColumnMinBookmarks(value: number) {
-			dispatch(setColumnMinBookmarks(id, value))
-		},
-	}
-})(ColumnSetting)
+export default connect(undefined, (dispatch: Dispatch, {id}: {id: number}) => ({
+	setColumnMinBookmarks(value: number) {
+		dispatch(setColumnMinBookmarks(id, value))
+	},
+}))(ColumnSetting)
