60e196d623d53dd1fb575a37fa103c8f43d40638
akameco <akameco.t@gmail.com>
2016-12-10 18:56:21 +0900

    Remove ColumnList

--- app/components/column/Column.js
+++ app/components/column/Column.js
@@ -1,9 +1,12 @@
 // @flow
 import React, {Component} from 'react'
+import {findDOMNode} from 'react-dom'
 import isEqual from 'lodash.isequal'
 import type {Illust} from '../../types/illust'
 import type {ColumnType} from '../../types/column'
-import ColumnList from './ColumnList'
+import Loading from '../common/Loading'
+import ColumnHeader from './ColumnHeader'
+import ColumnContent from './ColumnContent'
 import styles from './Column.css'
 
 type Props = {
@@ -14,23 +17,66 @@ type Props = {
 	onNextPage: () => void
 };
 
+type State = {
+	toTop: bool;
+};
+
 export default class Column extends Component {
 	props: Props;
+	target: Component<*, *, *>
+	root: typeof ColumnContent
+
+	state: State = {
+		toTop: false,
+	}
 
 	shouldComponentUpdate(nextProps: Props) {
 		return !isEqual(this.props.illusts, nextProps.illusts)
 	}
 
+	handleTopClick = (e: Event) => {
+		e.preventDefault()
+		const node: HTMLElement = findDOMNode(this.root)
+		if (node) {
+			node.scrollTop = 0
+		}
+		this.props.onReload()
+	}
+
 	render() {
-		const {column, ...othres} = this.props
+		const {
+			column,
+			illusts,
+			onClose,
+			onNextPage,
+		} = this.props
 		return (
-			<div className={styles.base}>
-				<ColumnList
+			<div className={styles.wrap}>
+				<ColumnHeader
 					id={column.id}
 					title={column.title}
-					{...othres}
+					onClose={onClose}
+					onTopClick={this.handleTopClick}
 					/>
+				{illusts.length > 0 ?
+					<ColumnContent
+						root={c => { // eslint-disable-line react/jsx-no-bind
+							this.target = c
+						}}
+						targetRef={c => { // eslint-disable-line react/jsx-no-bind
+							this.root = c
+						}}
+						onIntersect={onNextPage}
+						illusts={illusts}
+						/> : <ColumnLoading/>
+				}
 			</div>
 		)
 	}
 }
+
+const ColumnLoading = () => (
+	<div className={styles.loading}>
+		<Loading wrapStyle={{background: '#121212'}}/>
+	</div>
+)
