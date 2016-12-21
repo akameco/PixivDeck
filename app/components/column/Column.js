c74710b2e21b36e538d4e6e7cf5adbd6ef602dcd
akameco <akameco.t@gmail.com>
2016-12-21 09:18:19 +0900

    Smooth scroll

--- app/components/column/Column.js
+++ app/components/column/Column.js
@@ -20,6 +20,16 @@ type State = {
 	toTop: bool;
 };
 
+const easeOutExpo = x =>
+	x === 1 ? 1 : 1 - Math.pow(2, -10 * x)
+
+const position = (start, end, elapsed, duration) => {
+	if (elapsed > duration) {
+		return end
+	}
+	return start + ((end - start) * easeOutExpo(elapsed / duration))
+}
+
 export default class Column extends Component {
 	props: Props;
 	target: Component<*, *, *>
@@ -32,10 +42,34 @@ export default class Column extends Component {
 	handleTopClick = (e: Event) => {
 		e.preventDefault()
 		const node: HTMLElement = findDOMNode(this.root)
+
 		if (node) {
-			node.scrollTop = 0
+			const clock = Date.now()
+			const duration = 700
+
+			let opacity = 1
+			const start = node.scrollTop
+
+			const step = () => {
+				const elapsed = Date.now() - clock
+				node.scrollTop = position(start, 0, elapsed, duration)
+
+				if (elapsed < duration) {
+					if (duration - elapsed < 150) {
+						opacity += 0.09
+					} else if (opacity > 0) {
+						opacity -= 0.06
+					}
+					node.style.opacity = `${opacity}`
+
+					requestAnimationFrame(step)
+				} else {
+					node.style.opacity = '1.0'
+					this.props.onReload()
+				}
+			}
+			step()
 		}
-		this.props.onReload()
 	}
 
 	render() {
