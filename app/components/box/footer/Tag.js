d43edb7ed98113b2d2e60715e4b266f135962dab
akameco <akameco.t@gmail.com>
2017-02-12 08:40:38 +0900

    Tweak

--- app/components/box/footer/Tag.js
+++ app/components/box/footer/Tag.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const TagWrapper = styled.a`
+const A = styled.a`
 	color: #5d89a8;
 	margin-left: 5px;
 	cursor: pointer;
@@ -16,9 +16,9 @@ type Props = {
 const Tag = ({name, onClick}: Props) => {
 	const handleClick = () => onClick(name)
 	return (
-		<TagWrapper onClick={handleClick}>
+		<A onClick={handleClick}>
 			#{name}
-		</TagWrapper>
+		</A>
 	)
 }
 
