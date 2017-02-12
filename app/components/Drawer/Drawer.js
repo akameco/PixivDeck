62b616adabb676345741b9a8fb50762fc3071d71
akameco <akameco.t@gmail.com>
2017-02-12 10:49:38 +0900

    Tweak

---
+++ app/components/Drawer/Drawer.js
@@ -0,0 +1,53 @@
+// @flow
+import React from 'react'
+import keycode from 'keycode'
+import EventListener from 'react-event-listener'
+import Overlay from 'components/Overlay'
+import StyledDrawer from './StyledDrawer'
+import DrawerWrapper from './DrawerWrapper'
+
+export type Props = {
+	open: bool,
+	children?: React$Element<any>,
+	onRequestClose: () => void,
+	width?: number,
+}
+
+type DefaultProps = {
+	width: number
+}
+
+class Drawer extends React.PureComponent<DefaultProps, Props, void> {
+	props: Props
+
+	static defaultProps: DefaultProps = {
+		width: 600,
+	}
+
+	handleKeyUp = (event: Event) => {
+		if (keycode(event) === 'esc') {
+			this.props.onRequestClose()
+		}
+	}
+
+	render() {
+		const {
+			open,
+			width,
+			children,
+			onRequestClose,
+		} = this.props
+
+		return (
+			<DrawerWrapper>
+				{open && <EventListener target="window" onKeyUp={this.handleKeyUp}/>}
+				<StyledDrawer open={open} x={width}>
+					{open && children}
+				</StyledDrawer>
+				<Overlay show={open} style={{zIndex: 800}} onClick={onRequestClose}/>
+			</DrawerWrapper>
+		)
+	}
+}
+
+export default Drawer
---
+++ app/components/Drawer/StyledDrawer.js
@@ -0,0 +1,20 @@
+// @flow
+import styled from 'styled-components'
+
+const StyledDrawer = styled.div`
+	width: ${props => props.x}px;
+	right: ${props => -props.x}px;
+	transform: ${props => props.open ? `translateX(-${props.x}px)` : 'translateX(0)'};
+	z-index: 900;
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	transition: transform .2s ease-out;
+	-webkit-transition: -webkit-transform .2s ease-out;
+	will-change: transform;
+	overflow-y: auto;
+	box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.15);
+	background-color: #fff;
+`
+
+export default StyledDrawer
