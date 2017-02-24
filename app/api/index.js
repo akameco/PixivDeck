ee1f9ebf64077f3412d0c0718032980654cb18c4
akameco <akameco.t@gmail.com>
2017-02-24 10:42:04 +0900

    Remove redux-thunk

--- app/api/index.js
+++ app/api/index.js
@@ -5,11 +5,12 @@ import {parseUrl} from './util'
 
 type FetchResponse = {
 	response: Object,
-	params: ?Params
+	params: ?Params,
+	nextUrl: ?string
 }
 
 class Api {
-	static login(username, password) {
+	static login(username: string, password: string) {
 		return Promise.resolve().then(() => Pixiv.login(username, password))
 	}
 
@@ -21,6 +22,7 @@ class Api {
 		return {
 			response: normalizeIllusts(res),
 			params: nextParams,
+			nextUrl: res.nextUrl,
 		}
 	}
 
@@ -31,12 +33,15 @@ class Api {
 	static async userFollowDelete(id: number) {
 		return await Pixiv.userFollowDelete(id)
 	}
-	static async userIllusts(id, type) {
+	static async userIllusts(id: number, type) {
 		return await Pixiv.userIllusts(id, {type})
 	}
 	static async illustBookmarkAdd(id: number, isPublic: bool = true): Promise<Object> {
 		const restrict = isPublic ? 'public' : 'private'
 		return await Pixiv.illustBookmarkAdd(id, {restrict})
+	}
+	static async userDetail(id: number) {
+		return await Pixiv.userDetail(id)
 	}
 }
 
--- app/types/drawer.js
+++ app/types/drawer.js
@@ -17,4 +17,6 @@ export type DrawerAction =
 	| {|type: 'DRAWER_ADD_USER', user: User|}
 	| {|type: 'DRAWER_ADD_PROFILE', profile: Profile|}
 	| {|type: 'DRAWER_SET_NEXT_URL', url: string, drawerType: DrawerType|}
-
+	| {|type: 'DRAWER_FETCH_ILLUST', id: number, drawerType: DrawerType|}
+	| {|type: 'DRAWER_NEXT_PAGE', drawerType: DrawerType|}
+	| {|type: 'FETCH_USER_DETAIL', id: number|}
