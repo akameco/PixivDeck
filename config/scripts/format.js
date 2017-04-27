97622dc990c4cdb1593bc155a2cc2346388124d2
akameco <akameco.t@gmail.com>
2017-04-27 17:00:29 +0900

    Tweak

--- config/scripts/format.js
+++ config/scripts/format.js
@@ -3,7 +3,15 @@ const execa = require('execa')
 
 const files = '{app/!(dist|node_modules)/,config}/**/*.js'
 
-execa.shell(
-  `prettier --write --single-quote --semi=false --jsx-bracket-same-line --trailing-comma es5 --print-width 80 "${files}"`,
-  { stdio: 'inherit' }
-)
+const cmd = [
+  'prettier',
+  '--write',
+  '--single-quote',
+  '--semi=false',
+  '--jsx-bracket-same-line',
+  '--trailing-comma',
+  'es5',
+  `"${files}"`,
+].join(' ')
+
+execa.shell(cmd, { stdio: 'inherit' }).catch(console.log)
