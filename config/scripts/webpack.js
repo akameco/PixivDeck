93c2a30d2d35e18bb26950a2d1f8f241c5cb3ff3
akameco <akameco.t@gmail.com>
2017-04-27 16:14:10 +0900

    Add webpack build script

---
+++ config/scripts/webpack.js
@@ -0,0 +1,40 @@
+'use strict'
+const path = require('path')
+const execa = require('execa')
+
+const nodeModulesPath = path.join(process.cwd(), 'node_modules')
+const configPath = path.join(process.cwd(), 'config', 'webpack')
+
+const webpackPath = path.join(nodeModulesPath, 'webpack/bin/webpack')
+const webpackServerPath = path.join(
+  nodeModulesPath,
+  'webpack-dev-server',
+  'bin',
+  'webpack-dev-server'
+)
+
+const node = 'node --trace-warnings -r babel-register'
+
+const getEnv = x => 'cross-env NODE_ENV=' + x
+const devEnv = getEnv('development')
+const prodEnv = getEnv('production')
+
+const webpackOpts = '--progress --profile --colors'
+const config = x =>
+  `--config ${path.join(configPath, `webpack.config.${x}.js`)}`
+
+const exec = (file, env, p = webpackPath) => {
+  const opts = p === webpackPath ? webpackOpts : ''
+  const cmd = `${env} ${node} ${p} ${config(file)} ${opts}`.trim()
+  execa.shell(cmd, { stdio: 'inherit' })
+}
+
+const runWebpack = {
+  'build-dll': () => exec('renderer.dev.dll', devEnv),
+  'build-main': () => exec('main.prod', prodEnv),
+  'build-renderer': () => exec('renderer.prod', prodEnv),
+  'hot-server': () => exec('renderer.dev', devEnv, webpackServerPath),
+}
+
+const input = process.argv.slice(2)[0]
+runWebpack[input]()
