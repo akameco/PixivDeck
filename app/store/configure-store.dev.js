dcee54f8112ee281cc58ebaf99a4b65bf4847ee0
akameco <akameco.t@gmail.com>
2017-06-13 04:33:53 +0900

    Remove redux-logger

--- app/store/configure-store.dev.js
+++ app/store/configure-store.dev.js
@@ -1,19 +1,11 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import createSagaMiddleware from 'redux-saga'
-import { createLogger } from 'redux-logger'
 import reducer from 'reducers'
 import mySaga from '../sagas'
 import storeWrapper from './wrapper'
 
 export default function configureStore(initialState: Object) {
   const middleware = []
-
-  const logger = createLogger({
-    level: 'info',
-    collapsed: true,
-  })
-
-  middleware.push(logger)
 
   const sagaMiddleware = createSagaMiddleware()
   middleware.push(sagaMiddleware)
