d48cb38092bda06214895bf1de4a67462885ce46
akameco <akameco.t@gmail.com>
2017-06-25 07:04:49 +0900

    Move styles

--- app/containers/AddColumnModal/AddColumnModal.js
+++ app/containers/AddColumnModal/AddColumnModal.js
@@ -1,30 +1,11 @@
 // @flow
 import React from 'react'
-import styled from 'styled-components'
 import { FormattedMessage } from 'react-intl'
 import * as ranking from 'constants/ranking'
 import Card from './Card'
 import LinkButton from './LinkButton'
 import messages from './messages'
-
-const Wrap = styled.div`
-	height: calc(100% - 1rem);
-	padding: 1rem;
-`
-
-const Content = styled.div`
-	display: flex;
-	flex-direction: row;
-	background: #f5f5f5;
-	padding-bottom: 10px;
-	overflow-y: auto;
-`
-
-const Header = styled.div`
-	font-weight: 500;
-	margin-left: 10px;
-	margin-bottom: 10px;
-`
+import { Content, Header, Wrap } from './styles'
 
 export type Props = {
   addBookmark: () => void,
---
+++ app/containers/AddColumnModal/styles.js
@@ -0,0 +1,21 @@
+// @flow
+import styled from 'styled-components'
+
+export const Wrap = styled.div`
+	height: calc(100% - 1rem);
+	padding: 1rem;
+`
+
+export const Content = styled.div`
+	display: flex;
+	flex-direction: row;
+	background: #f5f5f5;
+	padding-bottom: 10px;
+	overflow-y: auto;
+`
+
+export const Header = styled.div`
+	font-weight: 500;
+	margin-left: 10px;
+	margin-bottom: 10px;
+`
