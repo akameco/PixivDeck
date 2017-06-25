5dcba92d44c6d28aa7230cea0695021752989b86
akameco <akameco.t@gmail.com>
2017-06-26 01:37:34 +0900

    BookmarkButton: tweak

--- app/components/BookmarkButton/index.js
+++ app/components/BookmarkButton/index.js
@@ -1,11 +1,9 @@
 // @flow
-import { connect } from 'react-redux'
-import type { Connector } from 'react-redux'
+import { connect, type Connector } from 'react-redux'
 import type { State, Dispatch } from 'types'
 import { addBookmark } from 'actions'
 import { getIllust } from 'reducers'
-import BookmarkButton from './BookmarkButton'
-import type { Props } from './BookmarkButton'
+import BookmarkButton, { type Props } from './BookmarkButton'
 
 type OwnProps = {
   id: number,
