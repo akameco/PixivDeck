4677da51e62377c068327b73dab558d3c5772fe1
akameco <akameco.t@gmail.com>
2017-07-02 07:22:07 +0900

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,11 @@ module.exports = {
     '^.+\\.js$': 'babel-jest',
   },
   moduleNameMapper: {
-    '^styleTheme$': '<rootDir>/app/styleTheme.js',
+    '^styles/(.*)$': '<rootDir>/app/styles/$1.js',
+    '^components/(.*)$': '<rootDir>/app/components/$1.js',
+    '^containers/(.*)$': '<rootDir>/app/containers/$1.js',
+    '^types/(.*)$': '<rootDir>/app/types/$1.js',
+    '^services/(.*)$': '<rootDir>/app/services/$1.js',
+    '^util/(.*)$': '<rootDir>/app/util/$1.js',
   },
 }
