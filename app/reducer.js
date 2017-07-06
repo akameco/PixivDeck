81cf86c2e9220c58ca4de731993143fb19aabd89
akameco <akameco.t@gmail.com>
2017-07-06 09:54:22 +0900

    Sort

--- app/reducer.js
+++ app/reducer.js
@@ -1,49 +1,49 @@
 // @flow
 import { combineReducers } from 'redux'
-import Language from 'containers/Language/reducer'
-import ModalManeger from 'containers/ModalManeger/reducer'
-import SettingModal from 'containers/SettingModal/reducer'
-import HeaderContainer from 'containers/HeaderContainer/reducer'
-import MangaPreview from 'containers/MangaPreview/reducer'
-import LoginModal from 'containers/LoginModal/reducer'
-import UserPopoverContainer from 'containers/UserPopoverContainer/reducer'
-import IllustPreview from 'containers/IllustPreview/reducer'
-import DrawerManager from 'containers/DrawerManager/reducer'
-import UserDrawerContainer from 'containers/UserDrawerContainer/reducer'
-import Table from 'containers/Table/reducer'
-import ColumnManager from 'containers/ColumnManager/reducer'
-import ColumnRanking from 'containers/ColumnRanking/reducer'
-import ColumnRankingR18 from 'containers/ColumnRankingR18/reducer'
 import ColumnBookmark from 'containers/ColumnBookmark/reducer'
 import ColumnFollow from 'containers/ColumnFollow/reducer'
 import ColumnHistory from 'containers/ColumnHistory/reducer'
-import ColumnUserIllust from 'containers/ColumnUserIllust/reducer'
+import ColumnManager from 'containers/ColumnManager/reducer'
+import ColumnRanking from 'containers/ColumnRanking/reducer'
+import ColumnRankingR18 from 'containers/ColumnRankingR18/reducer'
 import ColumnSearch from 'containers/ColumnSearch/reducer'
-import SearchField from 'containers/SearchField/reducer'
+import ColumnUserIllust from 'containers/ColumnUserIllust/reducer'
+import DrawerManager from 'containers/DrawerManager/reducer'
+import HeaderContainer from 'containers/HeaderContainer/reducer'
 import IllustById from 'containers/IllustById/reducer'
+import IllustPreview from 'containers/IllustPreview/reducer'
+import Language from 'containers/Language/reducer'
+import LoginModal from 'containers/LoginModal/reducer'
+import MangaPreview from 'containers/MangaPreview/reducer'
+import ModalManeger from 'containers/ModalManeger/reducer'
+import SearchField from 'containers/SearchField/reducer'
+import SettingModal from 'containers/SettingModal/reducer'
+import Table from 'containers/Table/reducer'
 import UserById from 'containers/UserById/reducer'
+import UserDrawerContainer from 'containers/UserDrawerContainer/reducer'
+import UserPopoverContainer from 'containers/UserPopoverContainer/reducer'
 
 export default combineReducers({
-  Language,
-  IllustById,
-  UserById,
-  ModalManeger,
-  SettingModal,
-  HeaderContainer,
-  MangaPreview,
-  LoginModal,
-  UserPopoverContainer,
-  IllustPreview,
-  DrawerManager,
-  UserDrawerContainer,
-  Table,
+  ColumnBookmark,
+  ColumnFollow,
+  ColumnHistory,
   ColumnManager,
   ColumnRanking,
   ColumnRankingR18,
-  ColumnBookmark,
-  ColumnFollow,
-  ColumnUserIllust,
-  ColumnHistory,
   ColumnSearch,
+  ColumnUserIllust,
+  DrawerManager,
+  HeaderContainer,
+  IllustById,
+  IllustPreview,
+  Language,
+  LoginModal,
+  MangaPreview,
+  ModalManeger,
   SearchField,
+  SettingModal,
+  Table,
+  UserById,
+  UserDrawerContainer,
+  UserPopoverContainer,
 })
--- app/sagas.js
+++ app/sagas.js
@@ -1,41 +1,41 @@
 // @flow
 // $FlowFixMe
 import { fork, all } from 'redux-saga/effects' // eslint-disable-line
-import UserDrawerContainer from 'containers/UserDrawerContainer/saga'
-import Table from 'containers/Table/saga'
-import ColumnRanking from 'containers/ColumnRanking/saga'
-import ColumnRankingR18 from 'containers/ColumnRankingR18/saga'
+import BookmarkButton from 'containers/BookmarkButton/saga'
+import BoxContainer from 'containers/BoxContainer/saga'
 import ColumnBookmark from 'containers/ColumnBookmark/saga'
 import ColumnFollow from 'containers/ColumnFollow/saga'
-import ColumnUserIllust from 'containers/ColumnUserIllust/saga'
-import ColumnSearch from 'containers/ColumnSearch/saga'
-import FollowButton from 'containers/FollowButton/saga'
-import BookmarkButton from 'containers/BookmarkButton/saga'
-import SearchField from 'containers/SearchField/saga'
-import UserPopoverContainer from 'containers/UserPopoverContainer/saga'
-import LoginModal from 'containers/LoginModal/saga'
-import BoxContainer from 'containers/BoxContainer/saga'
-import SettingModal from 'containers/SettingModal/saga'
 import ColumnHistory from 'containers/ColumnHistory/saga'
+import ColumnRanking from 'containers/ColumnRanking/saga'
+import ColumnRankingR18 from 'containers/ColumnRankingR18/saga'
+import ColumnSearch from 'containers/ColumnSearch/saga'
+import ColumnUserIllust from 'containers/ColumnUserIllust/saga'
+import FollowButton from 'containers/FollowButton/saga'
+import LoginModal from 'containers/LoginModal/saga'
+import SearchField from 'containers/SearchField/saga'
+import SettingModal from 'containers/SettingModal/saga'
+import Table from 'containers/Table/saga'
+import UserDrawerContainer from 'containers/UserDrawerContainer/saga'
+import UserPopoverContainer from 'containers/UserPopoverContainer/saga'
 
 function* root(): Generator<*, void, void> {
   yield all([
-    fork(UserDrawerContainer),
-    fork(Table),
-    fork(ColumnRanking),
-    fork(ColumnRankingR18),
+    fork(BookmarkButton),
+    fork(BoxContainer),
     fork(ColumnBookmark),
     fork(ColumnFollow),
-    fork(ColumnUserIllust),
-    fork(ColumnSearch),
-    fork(FollowButton),
-    fork(BookmarkButton),
-    fork(SearchField),
-    fork(UserPopoverContainer),
-    fork(LoginModal),
-    fork(BoxContainer),
-    fork(SettingModal),
     fork(ColumnHistory),
+    fork(ColumnRanking),
+    fork(ColumnRankingR18),
+    fork(ColumnSearch),
+    fork(ColumnUserIllust),
+    fork(FollowButton),
+    fork(LoginModal),
+    fork(SearchField),
+    fork(SettingModal),
+    fork(Table),
+    fork(UserDrawerContainer),
+    fork(UserPopoverContainer),
   ])
 }
 
