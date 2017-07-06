7af17737e0a52c2bec5f838f8eed0f7371737d90
akameco <akameco.t@gmail.com>
2017-07-06 22:13:51 +0900

    Remove flat

--- app/i18n/index.js
+++ app/i18n/index.js
@@ -1,6 +1,5 @@
 // @flow
 import { addLocaleData } from 'react-intl'
-import { flatten } from 'flat'
 import jaLocaleData from 'react-intl/locale-data/ja'
 import enLocaleData from 'react-intl/locale-data/en'
 
@@ -13,6 +12,6 @@ addLocaleData(jaLocaleData)
 export const appLocales = ['en', 'ja']
 
 export const translationMessages = {
-  en: flatten(enTranslationMessages),
-  ja: flatten(jaTranslationMessages),
+  en: enTranslationMessages,
+  ja: jaTranslationMessages,
 }
