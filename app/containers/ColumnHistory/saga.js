54bc1476ceded93b8ff597ceb51fe82eae8e59c2
akameco <akameco.t@gmail.com>
2017-07-09 06:58:39 +0900

    Add history test

--- app/containers/ColumnHistory/saga.js
+++ app/containers/ColumnHistory/saga.js
@@ -6,11 +6,13 @@ import { put, takeEvery } from 'redux-saga/effects'
 import { OPEN_ILLUST_VIEWER } from '../IllustPreview/constants'
 import { OPEN_MANGA_PREVIEW } from '../MangaPreview/constants'
 
-function* addHistoryColumn() {
+export function* addHistoryColumn(): Generator<*, void, void> {
   yield put(addColumn('HISTORY', { columnId: 'history', type: 'HISTORY' }))
 }
 
-function* addHistory({ id }) {
+type Action = { id: number }
+
+export function* addHistory({ id }: Action): Generator<*, void, void> {
   yield put(actions.addHistory(id))
 }
 
---
+++ app/containers/ColumnHistory/tests/sagas.test.js
@@ -0,0 +1,29 @@
+// @flow
+import { takeEvery, put } from 'redux-saga/effects'
+import * as sagas from '../saga'
+import * as Actions from '../constants'
+import * as actions from '../actions'
+import { OPEN_ILLUST_VIEWER } from 'containers/IllustPreview/constants'
+import { OPEN_MANGA_PREVIEW } from 'containers/MangaPreview/constants'
+
+test('root Saga', () => {
+  const gen = sagas.default()
+
+  let next = gen.next()
+  expect(next.value).toEqual(
+    takeEvery(Actions.ADD_COLUMN_HISTORY, sagas.addHistoryColumn)
+  )
+
+  next = gen.next()
+  expect(next.value).toEqual(takeEvery(OPEN_ILLUST_VIEWER, sagas.addHistory))
+
+  next = gen.next()
+  expect(next.value).toEqual(takeEvery(OPEN_MANGA_PREVIEW, sagas.addHistory))
+})
+
+test('add history', () => {
+  const gen = sagas.addHistory({ id: 1 })
+
+  const next = gen.next()
+  expect(next.value).toEqual(put(actions.addHistory(1)))
+})
