97e79448ce55c446ae35b4d724095f8ad33d245e
akameco <akameco.t@gmail.com>
2018-08-01 21:06:57 +0900

    style(prettier): exec yarn fmt

--- app/components/A/example.js
+++ app/components/A/example.js
@@ -6,7 +6,9 @@ import A from './index'
 storiesOf('A', module).add('basic', () => {
   return (
     <div>
-      Hello<A>Hello</A>Hello
+      Hello
+      <A>Hello</A>
+      Hello
     </div>
   )
 })
