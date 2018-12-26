ee1416f59c97ce9935cf9d1aad2d3d9c6dc8dd2d
maple3142 <kirby741852963@gmail.com>
2018-12-26 19:13:06 +0800

    Add tweetdeck style scrollbar

--- app/components/IllustList/styles.js
+++ app/components/IllustList/styles.js
@@ -7,4 +7,17 @@ export const ScrollableY = styled.div`
   overflow-y: scroll;
   overflow-x: hidden;
   height: 100%;
+  &::-webkit-scrollbar {
+    width: 11px;
+  }
+  &::-webkit-scrollbar-thumb {
+    min-height: 50px;
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 5px;
+    background-color: #444448;
+  }
+  &::-webkit-scrollbar-track {
+    border-left: 1px solid #444448;
+  }
 `
