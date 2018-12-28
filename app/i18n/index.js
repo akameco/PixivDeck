e8cccc86616a0809702b0860b610999734bcaf1c
iqbal rifai <iqbalrifai@waifu.club>
2018-12-28 20:04:39 +0700

    Add Indonesia Tanslation

--- app/i18n/index.js
+++ app/i18n/index.js
@@ -3,22 +3,26 @@ import { addLocaleData } from 'react-intl'
 import jaLocaleData from 'react-intl/locale-data/ja'
 import enLocaleData from 'react-intl/locale-data/en'
 import zhLocaleData from 'react-intl/locale-data/zh'
+import idLocaleData from 'react-intl/locale-data/id'
 import zhtwLocaleData from './localedata-zhtw'
 
 import enTranslationMessages from './en.yml'
+import idTranslationMessages from './id.yml'
 import jaTranslationMessages from './ja.yml'
 import zhTranslationMessages from './zh.yml'
 import zhtwTranslationMessages from './zh-TW.yml'
 
 addLocaleData(enLocaleData)
+addlocaleData(idLocaleData)
 addLocaleData(jaLocaleData)
 addLocaleData(zhLocaleData)
 addLocaleData(zhtwLocaleData)
 
-export const appLocales = ['en', 'ja', 'zh', 'zh-tw']
+export const appLocales = ['en', 'id', 'ja', 'zh', 'zh-tw']
 
 export const translationMessages = {
   en: enTranslationMessages,
+  id: idTranslationMessages,
   ja: jaTranslationMessages,
   zh: zhTranslationMessages,
   'zh-tw': zhtwTranslationMessages,
