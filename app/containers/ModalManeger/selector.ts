36b219a4a97ff755fb982d43c28c012326b3de3b
akameco <akameco.t@gmail.com>
2019-03-10 04:24:57 +0900

    refactor(typescript): selector

--- app/containers/ModalManeger/selector.ts
+++ app/containers/ModalManeger/selector.ts
@@ -1,6 +1,5 @@
-// @flow
 import { createSelector } from 'reselect'
-import type { State } from 'types/state'
+import { State } from 'types/state'
 
 const selectModalManeger = (state: State) => state.ModalManeger
 
